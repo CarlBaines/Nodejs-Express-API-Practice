658cbe3ed7ed1e2701782629503f374c62371807
Carl Baines <carltbaines@hotmail.co.uk>
2025-06-20 12:01:12 +0100

    Added a PUT and DELETE Requests

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.listen(
 This creates the API but express returns a 404 error code because there isn't any API endpoints set up yet.
 */
 
+// GET request - retrieves data from the server.
+
 // Creates an endpoint for the API.
 // Sets up the server with the 'drake' endpoint.
 // The request is handled by passing a callback function with request and repsonse objects as the second parameter.
@@ -27,6 +29,8 @@ app.get('/drake', (request, response) => {
         album: 'Views'
     })
 });
+
+// POST request - sends data to the server to create, update or delete a resource.
 
 // 'Create'/ add a new Drake song, in this case, to the server.
 app.post('/drake/:id', (request, response) => {
@@ -48,4 +52,44 @@ app.post('/drake/:id', (request, response) => {
     response.status(200).send({
         message: 'The Drake song: ' + songTitle + ' from ' + album + ' was added with the id: ' + id
     });
+});
+
+// Mock database for PUT method manipulation.
+let songs = [
+    { id: 1, songName: 'NOKIA', artist: 'Drake'},
+    { id: 2, songName: 'Not Like Us', artist: 'Kendrick Lamar'},
+    { id: 3, songName: 'One Dance', artist: 'Drake'}
+];
+
+// PUT request - used to send data to the server to create/update a resource.
+app.put('/songs/:id', (request, response) => {
+    const songID = parseInt(request.params.id);
+    const {songName, artist} = request.body;
+    // The .find method returns the first element in the array that matches the condition.
+    // The arrow function takes each song object from the array and checks if the song's ID is equal to the desired songID.
+    const song = songs.find(s => s.id === songID);
+
+    if(song){
+        // update song record in mock database.
+        song.songName = songName;
+        song.artist = artist;
+        response.send(songs);
+    }
+    else{
+        response.status(404).send('Song was not found');
+    }
+});    
+
+// DELETE request - used to remove resources from the server.
+app.delete('/songs/:id', (request, response) =>{
+    console.log('DELETE request received for ID:', request.params.id); // Add this line
+    const songID = parseInt(request.params.id);
+    const songIndex = songs.findIndex(s => s.id === songID);
+    if(songIndex != -1){
+        songs.splice(songIndex, 1);
+        response.send(songs);
+    }
+    else{
+        response.status(404).send('Song was not found for deletion');
+    }
 });
